090102cec55ca4dee51a98039cbf7dacd8a42eec
HarlandLohora <lohoraharland@gmail.com>
2018-12-06 12:26:41 -0600

    About & Link

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default () => (
+  <div>
+    <Link href="/">
+      <a>Home</a>
+    </Link>
+    <h1>About me</h1>
+    <p>Full Stack Web Developer</p>
+    <img src="/static/img/HarlandLohora.jpg" alt="Harland Lohora" width="250"/>
+  </div>
+);
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
+import Link from 'next/link';
 const Index = () => (
   <div>
-    <h1>Tu Canton</h1>
+    <Link href="/about">
+      <a>About Me</a>
+    </Link>
+    <h1>Home</h1>
+    <p>Welcome to the home page</p>
   </div>
 );
 
