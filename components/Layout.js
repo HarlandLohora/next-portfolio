6bff7a0b88dac693b674ef9d0fa96c4b8201ed7b
HarlandLohora <lohoraharland@gmail.com>
2018-12-06 16:00:42 -0600

    Agregando FAVICON y JSX para estilos

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,11 @@
 import Link from 'next/link';
+import Head from 'next/head';
 export default ({ title, children }) => (
   <div className="root">
+    <Head>
+      <title>Harland Lohora</title>
+      <link rel="icon" href="/static/img/favicon.png"/>
+    </Head>
     <header>
     <Link href="/"><a>Home </a></Link>
     <Link href="/about"><a>About </a></Link>
@@ -17,7 +22,7 @@ export default ({ title, children }) => (
         justify-content: center;
       }
       header{
-        background     : indigo;
+        background     : #03A9F4;
         display        : flex;
         font-size      : 1.2rem;
         justify-content: space-around;
@@ -25,11 +30,11 @@ export default ({ title, children }) => (
         width          : 97.2%;
       }
       header a{
-        color          : darkgrey;
+        color          : #FFF;
         text-decoration: none;
       }
       header a:hover{
-        color      : lightgrey;
+        color      : #111;
         font.weight: bold;
       }
       footer{
